e78ef7ac7771e82e0ffaceea86cac21675e3f3df
uzair <uzairkhanuk3215@gmail.com>
2022-04-07 22:33:47 +0530

    final file for finding square root of a number

--- sqrtcpp/sqrt.cpp
+++ sqrtcpp/sqrt.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+int intpart(int n);
+double squareroot(int n, int intpart, int precision);
 using namespace std;
+int main() {
+    int n = 37;
+    cout <<" square root of "<< n << "is " << squareroot(n, intpart(n), 3);
+}
 
+int intpart(int n) {
+    int s = 0;
+    int e = n;
+    long long int mid = s + (e - s) / 2;
+    int ans = -1;
+    
+    while (s <= e){
+        long long int square = mid*mid;
+        if (square == n) {
+            return mid;
+        } 
+        if (square < n) {
+            ans = mid;
+            s = mid + 1;
+        } else {
+            e =  mid - 1;
+        }
+        mid = s + (e - s) / 2;
+    }
+    return ans;
+}
 double squareroot(int n, int intpart, int precision) {
     double factor = 1;
     double ans =  intpart;
