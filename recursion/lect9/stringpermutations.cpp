5799401136d41a59c82699adabe129f224988333
uzair <uzairkhanuk3215@gmail.com>
2023-06-11 04:05:07 +0530

    i did this question on my own and also corrected some bugs

--- recursion/lect9/stringpermutations.cpp
+++ recursion/lect9/stringpermutations.cpp
@@ -1,3 +1,37 @@
 #include <iostream>
-using namespace std;
+#include <bits/stdc++.h>
+#include <string>
+using  namespace std;
+void swap(string &a, string&b) {
+    string temp = a;
+    a = b;
+    b = temp;
+}
+
+void generator(vector <string>& permSet, int index, string seq, string inp) {
+    if(index >= inp.length()){
+        permSet.push_back(seq);
+        return;
+    }
+    for (int j = index; j <inp.length(); j++) {
+        swap(inp[index], inp[j]);
+        generator(permSet, index+1, seq+inp[index], inp);
+        swap(inp[index], inp[j]);
+    }
+}
+vector <string> stringPermutation(string inp) {
+    vector <string> permutationSet;
+    int index = 0;
+    string sequence = "";
+    generator(permutationSet, index, sequence, inp);
+    return permutationSet;
+}
+int main () {
+    string input;
+    getline(cin, input);
+    vector<string> answer = stringPermutation(input);
+    for (auto i: answer) {
+        std :: cout << i << ' ';
+    }
+}
 
