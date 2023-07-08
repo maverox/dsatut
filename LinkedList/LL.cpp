b91a9043e28d1b88ad8a3f806222ac5441d565f1
uzair <uzairkhanuk3215@gmail.com>
2023-07-08 23:04:32 +0530

    used a new approach and documented it as well with comments

--- LinkedList/LL.cpp
+++ LinkedList/LL.cpp
@@ -1,16 +1,78 @@
 #include <iostream>
 using namespace std;
-class Node
-{
+// Node class represents a single node in the linked list
+class Node {
 public:
-    int data;
-    Node *next;
-    
-    Node(int data) {
-        this->data = data;
-        this->next = NULL;
+    int data;       // Data stored in the node
+    Node* next;     // Pointer to the next node
+
+    // Constructor
+    Node(int value) : data(value), next(nullptr) {} //The initialization list : data(value), next(nullptr) follows the constructor declaration and specifies how the member variables of the class should be initialized. In this case, it initializes the data member with the value passed to the constructor (value)
+};
+
+// Linked list class
+class LinkedList {
+private:
+    Node* head;     // Pointer to the head of the list
+
+public:
+    // Constructor
+    LinkedList() : head(nullptr) {}
+
+    // Destructor
+    ~LinkedList() {
+        // Delete all nodes in the list
+        Node* current = head;
+        while (current != nullptr) {
+            Node* temp = current;
+            current = current->next;
+            delete temp;
+        }
     }
-    void insertAthead(Node* &head, Node* &tail, int data) {
-        
+
+    // Function to add a new node at the beginning of the list
+    void prepend(int value) {
+        Node* newNode = new Node(value);
+        newNode->next = head;
+        head = newNode;
     }
+
+    // Function to add a new node at the end of the list
+    void append(int value) {
+        Node* newNode = new Node(value);
+        if (head == nullptr) {
+            head = newNode;
+        } else {
+            Node* current = head;
+            while (current->next != nullptr) {
+                current = current->next;
+            }
+            current->next = newNode;
+        }
+    }
+
+    // Function to display the elements in the list
+    void display() {
+        Node* current = head;
+        while (current != nullptr) {
+            cout << current->data << " ";
+            current = current->next;
+        }
+        cout << endl;
+    }
+};
+
+// Test the linked list implementation
+int main() {
+    LinkedList list;
+
+    list.append(1);
+    list.append(2);
+    list.append(3);
+
+    list.prepend(0);
+
+    list.display();
+
+    return 0;
 }
